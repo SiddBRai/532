49a50d676acbe24f961cf66caf6624661db232af
cuc1057 <cuc1057@psu.edu>
2020-01-30 21:44:33 -0500

    profile gpu

--- apriori.cu
+++ apriori.cu
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <assert.h>
+#include <unistd.h>
+
+#include <cuda_profiler_api.h>
 
 #include <vector>
 #include <unordered_map>
@@ -10,6 +13,8 @@
 #include <fstream>
 #include <numeric>
 #include <functional>
+#include <set>
+#include <chrono>
 
 //#include "lock.h"
 
@@ -29,15 +34,15 @@ typedef struct {
 	int item_no;
     int freq;
     int trans_array_size;
-    int trans_array[256];
+    int trans_array[128];
 } Item;
 
 typedef struct {
     int freq;
     int item_set_size;
-	int item_set_code[256];
+	int item_set_code[16];
     int trans_array_size;
-    int trans_array[256];
+    int trans_array[16];
 
     /* the indices of previous sets */
     int set1_index;
@@ -52,8 +57,10 @@ typedef struct {
     int *item_code_array;
 } SupportCount;
 
-#define TRANS_NUM 600000
-#define NUM_THREADS 16
+#define TRANS_NUM 1000
+#define ITEM_NUM 2000
+#define NUM_THREADS 1
+#define BLOCK_SIZE 1
 
 
 __global__ 
@@ -170,7 +177,7 @@ void find_support_count(int candidateSetSize, ItemSet* candidateSet, int* global
     }
     
     /* block-level barrier */
-    __syncthreads();
+    //__syncthreads();
 }
 
 int itemcodeComp(const void* a, const void* b)
@@ -194,6 +201,16 @@ int itemsetComp(const void* a, const void* b)
     return 0;
 }
 
+bool hasTheItemSet(std::set<ItemSet*>& itemsets_set, ItemSet* checked_set) 
+{
+    for (auto it = itemsets_set.begin(); it != itemsets_set.end(); it++) {
+        if ((*it)->item_set_size != checked_set->item_set_size) continue;
+        if (memcmp((*it)->item_set_code, checked_set->item_set_code, checked_set->item_set_size*sizeof(int)) == 0) {
+            return true; 
+        }
+    }
+    return false;
+}
 
 int find_last_eq_class_item(int array_size, ItemSet* itemset_array, int base_pos, int start_pos, int cardinality)
 {
@@ -233,6 +250,7 @@ void* genNextItemSetArray(int itemset_array_size, ItemSet* curr_itemset_array, i
     if (nextCardinality == 2) {
         int next_size = (_arr_size*(_arr_size-1)) / 2;
         next_set = (ItemSet*)malloc(next_size*sizeof(ItemSet));
+        assert(next_set != NULL);
         memset(next_set, 0, next_size*sizeof(ItemSet));
         for (int i = 0; i < _arr_size-1; i++) {
             for (int j = i+1; j < _arr_size; j++) {
@@ -271,7 +289,13 @@ void* genNextItemSetArray(int itemset_array_size, ItemSet* curr_itemset_array, i
         };
         /* allocate next level item set memory */ 
         int next_size = pairSum(ranges_vec);
-        next_set = (ItemSet*)malloc(next_size*sizeof(ItemSet));
+        if (next_size <= 0 || next_size > 1024*1024) {
+            return NULL;
+        }
+
+        set<ItemSet*> itemsets_set;
+        next_set = (ItemSet*)malloc((size_t)next_size*(size_t)sizeof(ItemSet));
+        assert(next_set != NULL);
         memset(next_set, 0, next_size*sizeof(ItemSet));
         for (auto range : ranges_vec) {
             /* the priori nextCardinality-2 items should be the same */
@@ -286,6 +310,14 @@ void* genNextItemSetArray(int itemset_array_size, ItemSet* curr_itemset_array, i
                     
                     next_set[new_idx].item_set_code[nextCardinality-1] = curr_itemset_array[end_pos].item_set_code[nextCardinality-2];
                     
+                    /*
+                    if (hasTheItemSet(itemsets_set, &(next_set[new_idx]))) {
+                        next_size--;
+                        continue;
+                    }
+                    itemsets_set.insert(&(next_set[new_idx]));
+                    */
+
                     /* store the indices */
                     next_set[new_idx].set1_index = start_pos;
                     next_set[new_idx].set2_index = end_pos;
@@ -300,7 +332,7 @@ void* genNextItemSetArray(int itemset_array_size, ItemSet* curr_itemset_array, i
     return (void*)next_set;
 }
 
-int main(void) 
+int main(int argc, char *argv[]) 
 {
     fstream fs;
     string line;
@@ -310,24 +342,52 @@ int main(void)
 
     int trans_count = 0;    /* number of transactions */
     int item_count = 0;     /* number of unique items */
-    int min_support = 3;    /* mininum supoort of items */
+    int min_support = 6;    /* mininum supoort of items */
+    
+    /* profiling */
+    size_t memory_use = 0, max_memory_use = 0;
+    size_t dev_memory_use = 0, max_dev_memory_use = 0;
+
+    /* args */
+    int opt;
+    int num_threads = 0;
+    int block_size = 0;
+    int item_num = ITEM_NUM;
+    const char *optstr = "n:b:i:";
+    while ((opt = getopt(argc, argv, optstr)) != -1) {
+        switch (opt) {
+            case 'n':
+                num_threads = atoi(optarg);
+                break;
+            case 'b':
+                block_size = atoi(optarg);
+                break;
+            case 'i':
+                item_num = atoi(optarg);
+                break;
+        }
+    }
+    printf("num threads %d, block size %d\n", num_threads, block_size); 
 
     Transaction *transArray = (Transaction*)malloc(TRANS_NUM*sizeof(Transaction));
     memset(transArray, 0, TRANS_NUM*sizeof(Transaction));
+    memory_use += TRANS_NUM*sizeof(Transaction);
 
     /* read from the file */
-    fs.open("test.csv", ios::in);
+    //fs.open("test.csv", ios::in);
+    fs.open("data.csv", ios::in);
     while (getline(fs, line)) {
+        if (line.size() == 0) continue;
         /* get transaction number */
         ssize_t pos = line.find(",");
         int trans_no = atoi(line.substr(0, pos).c_str());
         ssize_t pos2 = line.find(",", pos+1);
         string item = line.substr(pos+1, pos2-pos-1);
-
+        
         /* find item number */
         if (item_code_map.find(item) == item_code_map.end()) {
             item_code_map[item] = item_count++;
-            printf("Item Count :%d -> %s\n", item_count, item.c_str());
+            //printf("Item Count :%d -> %s\n", item_count, item.c_str());
         }
         /* find transaction number */
         if (transaction_map.find(trans_no) == transaction_map.end()) {
@@ -335,18 +395,52 @@ int main(void)
             transArray[trans_count].item_code[transArray[trans_count].item_size++] = item_code_map[item];
             transaction_map[trans_no] = trans_count;
             trans_count++;
+            //printf("Transaction Count :%d -> %d\n", trans_count, trans_no);
         }
-        else {
+        //else 
+        {
             int _idx = transaction_map[trans_no]; 
+            auto checkItemExist = [](Transaction* _tr, int _code) -> bool
+            {
+                bool ret = false;
+                for (int idx = 0; idx < _tr->item_size; idx++) {
+                    if (_tr->item_code[idx] == _code) return true;
+                }
+                return ret;
+            };
+            //if (!checkItemExist(&(transArray[_idx]), item_code_map[item])) 
             transArray[_idx].item_code[transArray[_idx].item_size++] = item_code_map[item];
         }
+
+        if (trans_count >= TRANS_NUM) break;
+        if (item_count >= item_num) break;
     }
     fs.close();
     
+    printf("Item Count: %d, Transaction Count: %d\n", item_count, trans_count);
+   
+    size_t total = 0;
+    auto begin = chrono::high_resolution_clock::now();
+
     /* sort item code array for each transaction */
     for (int _tr_idx = 0; _tr_idx < trans_count; _tr_idx++) {
         qsort(transArray[_tr_idx].item_code, transArray[_tr_idx].item_size, sizeof(int), itemcodeComp);
+        int glb_i = 0;
+        int i;
+        for (i = 0; i < transArray[_tr_idx].item_size-1; i++) {
+            while (i < transArray[_tr_idx].item_size && transArray[_tr_idx].item_code[i] == transArray[_tr_idx].item_code[i+1]) {
+                i++;
+            }
+            transArray[_tr_idx].item_code[glb_i++] = transArray[_tr_idx].item_code[i];
+        }
+        if (i == transArray[_tr_idx].item_size-1) {
+            transArray[_tr_idx].item_code[glb_i++] = transArray[_tr_idx].item_code[i-1];
+        }
+        transArray[_tr_idx].item_size = glb_i;
     }
+    
+    auto end = chrono::high_resolution_clock::now();
+    total += chrono::duration_cast<chrono::milliseconds>(end-begin).count();
  
     auto printTrans = [](int _arr_size, Transaction* _trans_array)
     {
@@ -357,11 +451,13 @@ int main(void)
             }   
         }
     };
-    printTrans(trans_count, transArray);
+    //printTrans(trans_count, transArray);
 
+    begin = chrono::high_resolution_clock::now();
 
     Item *itemArray = (Item*)malloc(item_count*sizeof(Item));
     memset(itemArray, 0, item_count*sizeof(Item));
+    memory_use += item_count*sizeof(Item);
     for (int i = 0; i < item_count; i++) {
         itemArray[i].item_no = i;
     }
@@ -370,14 +466,18 @@ int main(void)
     Transaction *dev_transArray = NULL;
     cudaMalloc(&dev_transArray, TRANS_NUM*sizeof(Transaction));
     cudaMemcpy(dev_transArray, transArray, TRANS_NUM*sizeof(Transaction), cudaMemcpyHostToDevice);
+    dev_memory_use += TRANS_NUM*sizeof(Item);
     
     Item *dev_itemArray = NULL;
     cudaMalloc(&dev_itemArray, item_count*sizeof(Item));
     cudaMemcpy(dev_itemArray, itemArray, item_count*sizeof(Item), cudaMemcpyHostToDevice);
+    dev_memory_use += item_count*sizeof(Item);
     
     /* calculate single item frequency */
-    dim3 gridSize(256);
-    dim3 blockSize(16);
+    int num_threads_per_block = (num_threads < block_size) ? num_threads : block_size;
+    //dim3 gridSize(num_threads/block_size);
+    dim3 gridSize(NUM_THREADS/BLOCK_SIZE);
+    dim3 blockSize(BLOCK_SIZE);
     item_freq_count<<<gridSize, blockSize>>>(trans_count, dev_transArray, dev_itemArray);
 
     /* copy the results back to host */
@@ -386,7 +486,22 @@ int main(void)
     /* sort transaction array for each item */
     for (int _it_idx = 0; _it_idx < item_count; _it_idx++) {
         qsort(itemArray[_it_idx].trans_array, itemArray[_it_idx].trans_array_size, sizeof(int), itemcodeComp);
+        int glb_i = 0;
+        int i;
+        for (i = 0; i < itemArray[_it_idx].trans_array_size-1; i++) {
+            while (i < itemArray[_it_idx].trans_array_size && itemArray[_it_idx].trans_array[i] == itemArray[_it_idx].trans_array[i+1]) {
+                i++;
+            }
+            itemArray[_it_idx].trans_array[glb_i++] = itemArray[_it_idx].trans_array[i];
+        }
+        if (i == itemArray[_it_idx].trans_array_size-1) {
+            itemArray[_it_idx].trans_array[glb_i++] = itemArray[_it_idx].trans_array[i-1];
+        }
+        itemArray[_it_idx].trans_array_size = glb_i;
     }
+    
+    end = chrono::high_resolution_clock::now();
+    total += chrono::duration_cast<chrono::milliseconds>(end-begin).count();
 
     /* check point of transposed database */
     auto printItems = [](int _arr_size, Item* _item_array)
@@ -398,8 +513,10 @@ int main(void)
             }   
         }
     };
-    printItems(item_count, itemArray);
+    //printItems(item_count, itemArray);
     
+    begin = chrono::high_resolution_clock::now();
+ 
     /* start to prune */
     int globalIdx = 0;
     int *dev_globalIdx = NULL;
@@ -410,17 +527,24 @@ int main(void)
 
     ItemSet *itemsetArray = (ItemSet*)malloc(item_count*sizeof(ItemSet));
     memset(itemsetArray, 0, item_count*sizeof(ItemSet));
+    memory_use += item_count*sizeof(ItemSet);
     
     ItemSet *dev_itemsetArray = NULL;
     cudaMalloc(&dev_itemsetArray, item_count*sizeof(ItemSet));
     cudaMemcpy(dev_itemsetArray, itemsetArray, item_count*sizeof(ItemSet), cudaMemcpyHostToDevice);
+    dev_memory_use += item_count*sizeof(ItemSet);
     
     /* kernel doing selection for single item with minimum support */
     select_with_min_support<<<gridSize, blockSize>>>(item_count, dev_itemArray, min_support, dev_itemsetArray, dev_globalIdx);
 
     cudaMemcpy(itemsetArray, dev_itemsetArray, item_count*sizeof(ItemSet), cudaMemcpyDeviceToHost);
     cudaMemcpy(&globalIdx, dev_globalIdx, sizeof(int), cudaMemcpyDeviceToHost);
+   
+    free(itemArray);
     
+    end = chrono::high_resolution_clock::now();
+    total += chrono::duration_cast<chrono::milliseconds>(end-begin).count();
+
     /* check point of transposed database */
     auto printItemSet = [](int _arr_size, ItemSet* _itemset_array)
     {
@@ -437,7 +561,7 @@ int main(void)
             printf("\tSet Index (%d,%d)\n", _itemset_array[_it_idx].set1_index, _itemset_array[_it_idx].set2_index);
         }
     };
-    printItemSet(globalIdx, itemsetArray);
+    //printItemSet(globalIdx, itemsetArray);
 
  
     /* Record in Support Count */
@@ -449,6 +573,7 @@ int main(void)
             sc.count = itemset_array[is_idx].item_set_size;
             sc.item_code_array = (int*)malloc(sc.count * sizeof(int));
             memcpy(sc.item_code_array, itemset_array[is_idx].item_set_code, sc.count*sizeof(int));
+            qsort(sc.item_code_array, sc.count, sizeof(int), itemcodeComp);
             vec.push_back(sc);
         }
     };   
@@ -474,24 +599,31 @@ int main(void)
 
     while (true) {
         candidateSet = (ItemSet*)genNextItemSetArray(currSetSize, currSet, cardinality, &candidateSetSize);
-        if (candidateSetSize == 0) {
+        if (candidateSetSize == 0 || candidateSet == NULL) {
             break;
         }
         assert(candidateSet != NULL);          
         
         printf("\n\n Next candidate size is %d\n", candidateSetSize);
+        memory_use += candidateSetSize*sizeof(ItemSet);
+        
+        begin = chrono::high_resolution_clock::now();
 
         /* allocate GPU kernel memory */
         cudaMemcpy(dev_candidateSetSize, &candidateSetSize, sizeof(int), cudaMemcpyHostToDevice);
         cudaMemcpy(dev_globalIdx, &globalIdx, sizeof(int), cudaMemcpyHostToDevice);
         cudaMalloc(&dev_currSet, currSetSize*sizeof(ItemSet));
+        assert(dev_currSet != NULL);
         cudaMemcpy(dev_currSet, currSet, currSetSize*sizeof(ItemSet), cudaMemcpyHostToDevice);
         cudaMalloc(&dev_candidateSet, candidateSetSize*sizeof(ItemSet));
         cudaMemcpy(dev_candidateSet, candidateSet, candidateSetSize*sizeof(ItemSet), cudaMemcpyHostToDevice);        
         
+        dev_memory_use += currSetSize*sizeof(ItemSet);
+        dev_memory_use += candidateSetSize*sizeof(ItemSet);
+
         /* launch the kernel */
-        dim3 gSize(256);
-        dim3 bSize(16);
+        dim3 gSize(NUM_THREADS/BLOCK_SIZE);
+        dim3 bSize(BLOCK_SIZE);
         find_support_count<<<gSize, bSize>>>(candidateSetSize, 
                                                      dev_candidateSet, 
                                                      dev_globalIdx, 
@@ -502,6 +634,9 @@ int main(void)
         /* copy the result back */
         cudaMemcpy(candidateSet, dev_candidateSet, candidateSetSize*sizeof(ItemSet), cudaMemcpyDeviceToHost);
         
+        end = chrono::high_resolution_clock::now();
+        total += chrono::duration_cast<chrono::milliseconds>(end-begin).count(); 
+        
         /* prune if freq == -1 */
         int _glb_set_idx = 0;
         for (int set_idx = 0; set_idx < candidateSetSize; set_idx++) {
@@ -511,7 +646,7 @@ int main(void)
             }
         }
         //candidateSetSize = _glb_set_idx;
-        printItemSet(_glb_set_idx, candidateSet); 
+        //printItemSet(_glb_set_idx, candidateSet); 
 
         /* Make statistics for support count */
         sc_record_func(support_count_vec, candidateSetSize, candidateSet);
@@ -519,19 +654,26 @@ int main(void)
         /* update the parameters and free previously used memory */
         free(currSet);
         cudaFree(dev_currSet);
+        
         cardinality++;
         currSet = candidateSet;
         currSetSize = candidateSetSize;
-        dev_currSet = dev_candidateSet; 
+        //dev_currSet = dev_candidateSet; 
+        cudaFree(dev_candidateSet);
         globalIdx = 0;
 
         if (_glb_set_idx <= 1) {
             break;
         }
+        
+        max_memory_use = (max_memory_use < memory_use) ? memory_use : max_memory_use;
+        max_dev_memory_use = (max_dev_memory_use < dev_memory_use) ? dev_memory_use : max_dev_memory_use;
+        memory_use -= currSetSize*sizeof(ItemSet);
+        dev_memory_use -= candidateSetSize*sizeof(ItemSet);
     }
     
     /* final result */
-    printItemSet(currSetSize, currSet); 
+    //printItemSet(currSetSize, currSet); 
     
     /* Finally generate association rules */
     auto get_support_count = [](vector<SupportCount>& vec, ItemSet* itemset)->int
@@ -539,7 +681,7 @@ int main(void)
         int _size = itemset->item_set_size;
         for (auto sc : vec) {
             if (sc.count != _size) continue;
-            if (memcmp(itemset->item_set_code, sc.item_code_array, _size) != 0) continue;
+            if (memcmp(itemset->item_set_code, sc.item_code_array, _size*sizeof(int)) != 0) continue;
             return sc.freq;
         }
         return 0;
@@ -552,21 +694,19 @@ int main(void)
         if (array_index+1 == size) {
             int _support_count = get_support_count(vec, sub_itemset);
             /* now we can calculate the confidence */
+            if (_support_count == 0) return;
+            //printf("freq %f, s_count %f\n", (float)(itemset->freq), (float)(_support_count));
             float confidence =  (float)(itemset->freq) / (float)(_support_count);
             
-            printf("\n\n------------------------------------------------------\n");
-            printf("------------------------------------------------------\n");
-            printf("-----------------Association Rules--------------------\n");
+            //printf("-----------------Association Rules--------------------\n");
             
-            printf("Items: \n");
-            for (int i = 0; i < size; i++) printf("\tItem %d\t", sub_itemset->item_set_code[i]);
-            printf("\nBase: \n");
-            for (int i = 0; i < itemset->item_set_size; i++) printf("\tItem %d\t", itemset->item_set_code[i]);
-            printf("\n\n=====  Confidence %f =====\n", confidence);
+            //printf("Items: \n");
+            //for (int i = 0; i < size; i++) printf("\tItem %d\t", sub_itemset->item_set_code[i]);
+            //printf("\nBase: \n");
+            //for (int i = 0; i < itemset->item_set_size; i++) printf("\tItem %d\t", itemset->item_set_code[i]);
+            //printf("\n\n=====  Confidence %f =====\n", confidence);
 
-            printf("------------------------------------------------------\n");
-            printf("------------------------------------------------------\n");
-            printf("------------------------------------------------------\n");
+            //printf("------------------------------------------------------\n");
             
             return;
         }
@@ -594,9 +734,15 @@ int main(void)
     for (int idx = 0; idx < currSetSize; idx++) {
         ItemSet* item_set = &currSet[idx];
         for (int _size = 1; _size <= item_set->item_set_size; _size++) {
-            getRules(item_set, _size, support_count_vec);
+            //getRules(item_set, _size, support_count_vec);
         }
     }
+    
+    cudaProfilerStop();    
+ 
+    printf("Sumary : Item Count %d --- Trans Count %d\nExec Time %llu ms\n", item_count, trans_count, total);
+    printf("\t CPU memory max usage : %llu bytes\n", max_memory_use);
+    printf("\t GPU memory max usage : %llu bytes\n", max_dev_memory_use);
 
     return 0;
 }
